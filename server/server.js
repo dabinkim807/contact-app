57142a23429cdf60e9f041deddbbd04907a4a951
Dana <dabinkim807@gmail.com>
2023-03-28 14:47:14 -0400

    add put route in server

--- server/server.js
+++ server/server.js
@@ -29,33 +29,25 @@ app.post('/api/contacts', async (req, res) => {
             'INSERT INTO contacts(name, email, phone, notes) VALUES($1, $2, $3, $4) RETURNING *',
             [req.body.name, req.body.email, req.body.phone, req.body.notes],
         );
-        console.log(result.rows[0]);
     } catch (e) {
         return res.status(400).json({ e });
     }
     return res.end();
 });
 
-// app.put('/api/students/:studentId', async (req, res) =>{
-//     //console.log(req.params);
-//     //This will be the id that I want to find in the DB - the student to be updated
-//     const studentId = req.params.studentId
-//     const updatedStudent = { id: req.body.id, firstname: req.body.firstname, lastname: req.body.lastname, iscurrent: req.body.is_current}
-//     console.log("In the server from the url - the student id", studentId);
-//     console.log("In the server, from the react - the student to be edited", updatedStudent);
-//     // UPDATE students SET lastname = "something" WHERE id="16";
-//     const query = `UPDATE students SET firstname=$1, lastname=$2, is_current=$3 WHERE id=${studentId} RETURNING *`;
-//     const values = [updatedStudent.firstname, updatedStudent.lastname, updatedStudent.iscurrent];
-//     try {
-//       const updated = await db.query(query, values);
-//       console.log(updated.rows[0]);
-//       res.send(updated.rows[0]);
-  
-//     }catch(e){
-//       console.log(e);
-//       return res.status(400).json({e})
-//     }
-//   })
+app.put('/api/contacts/:contactID', async (req, res) => {
+    const id = parseInt(req.params.contactID);
+	try {
+		const result = await db.query(
+			"UPDATE contacts SET name = $1, email = $2, phone = $3, notes = $4 WHERE id = $5 RETURNING *", 
+			[req.body.name, req.body.email, req.body.phone, req.body.notes, id]
+		);
+        console.log(result.rows[0]);
+	} catch(error) {
+		console.log(error);
+	}
+	return res.end();
+})
 
 // app.delete('/api/students/:studentId', async (req, res) => {
 //     try {
