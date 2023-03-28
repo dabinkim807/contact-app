3b17b51fb1eeb2ec545ce0650bbb9337f7d8849e
Dana <dabinkim807@gmail.com>
2023-03-28 15:15:36 -0400

    add delete route to server

--- server/server.js
+++ server/server.js
@@ -26,17 +26,17 @@ app.get('/api/contacts', async (req, res) => {
 app.get('/api/contacts/:contactID', async (req, res) => {
     const id = parseInt(req.params.contactID);
     try {
-        const { rows: contacts } = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
+        const { rows: contacts } = await db.query("SELECT * FROM contacts WHERE id = $1", [id]);
         res.send(contacts);
     } catch (e) {
-        return res.status(400).json(res.rows);
+        return res.status(400).json({ e });
     }
 });
 
 app.post('/api/contacts', async (req, res) => {
     try {
         await db.query(
-            'INSERT INTO contacts(name, email, phone, notes) VALUES($1, $2, $3, $4) RETURNING *',
+            "INSERT INTO contacts(name, email, phone, notes) VALUES($1, $2, $3, $4) RETURNING *",
             [req.body.name, req.body.email, req.body.phone, req.body.notes],
         );
     } catch (e) {
@@ -58,18 +58,15 @@ app.put('/api/contacts/:contactID', async (req, res) => {
 	return res.end();
 })
 
-// app.delete('/api/students/:studentId', async (req, res) => {
-//     try {
-//         const studentId = req.params.studentId;
-//         await db.query('DELETE FROM students WHERE id=$1', [studentId]);
-//         console.log("From the delete request-url", studentId);
-//         res.status(200).end();
-//     } catch (e) {
-//         console.log(e);
-//         return res.status(400).json({ e });
-
-//     }
-// });
+app.delete('/api/contacts/:contactID', async (req, res) => {
+    const id = parseInt(req.params.contactID);
+	try {
+		await db.query("DELETE FROM contacts WHERE id = $1", [id]);
+	} catch(error) {
+		return res.status(400).json({ e });
+	}
+	return res.end();
+});
 
 
 app.listen(PORT, () => {
